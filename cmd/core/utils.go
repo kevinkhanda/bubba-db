bd156488f393aec493b8ebc435bbc9f109fd1635
Your Name <sergey_umnov@mail.ru>
2018-04-04 14:33:19 +0300

    Implement boolToByteArray and byteArrayToBool conversions

--- cmd/core/utils.go
+++ cmd/core/utils.go
@@ -26,12 +26,43 @@ func int32ToByteArray(number int32) []byte {
 // byteArrayToInt32 transforms byte array of size 4 to int32
 // Returns int32 value and error if length of byte array != 4
 // Reversed conversion is possible only with int32ToByteArray function
-func byteArrayToInt32(byte []byte) (int32, error) {
-	if len(byte) != 4 {
-		errorMessage := fmt.Sprintf("wrong byte array length. expected array length is 4, actual length is %d", len(byte))
+func byteArrayToInt32(bs []byte) (int32, error) {
+	if len(bs) != 4 {
+		errorMessage := fmt.Sprintf("wrong byte array length. expected array length is 4, actual length is %d", len(bs))
 		return -1, errors.New(errorMessage)
 	}
-	unsigned := binary.LittleEndian.Uint32(byte)
+	unsigned := binary.LittleEndian.Uint32(bs)
 	number := int32(unsigned)
 	return number, nil
+}
+
+// boolToByteArray transforms bool value to byte array
+// Returns byte array of size 1
+// Reversed conversion is possible only with byteArrayToBool function
+func boolToByteArray(flag bool) []byte {
+	bs := make([]byte, 1)
+	if flag {
+		bs[0] = 0x01
+	} else {
+		bs[0] = 0x00
+	}
+	return bs
+}
+
+// byteArrayToBool tansforms byte array of size 1 to bool
+// Returns bool value and error if byte array size is not 1 or it contains bad data
+// Reversed conversion is possible only with boolToByteArray function
+func byteArrayToBool(bs []byte) (bool, error)  {
+	if len(bs) != 1 {
+		errorMessage := fmt.Sprintf("wrong byte array length. expected array length is 1, actual length is %d", len(bs))
+		return false, errors.New(errorMessage)
+	}
+	if bs[0] == 0x00 {
+		return false, nil
+	} else if bs[0] == 0x01 {
+		return true, nil
+	} else {
+		errorMessage := fmt.Sprintf("byte array contains bad data")
+		return false, errors.New(errorMessage)
+	}
 }
