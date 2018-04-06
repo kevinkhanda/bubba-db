1be60e50d9b720c8aa7eaac8af06d7b96784113b
Your Name <sergey_umnov@mail.ru>
2018-04-06 16:45:22 +0300

    Change FileHandler usage in tests

--- internal/app/core/core_test.go
+++ internal/app/core/core_test.go
@@ -7,9 +7,9 @@ import (
 	"io/ioutil"
 	"graph-db/internal/app/core/globals"
 )
+var fh FileHandler
 
 func init()  {
-	var fh FileHandler
 	globals.FileHandler = fh
 }
 
@@ -22,13 +22,13 @@ func TestFileReadWrite(test *testing.T) {
 	defer testFile.Close()
 	defer os.Remove(testFile.Name())
 	bs := []byte{53, 57, 50, 54}
-	err = globals.FileHandler.Write(testFile, 0, bs)
+	err = fh.Write(testFile, 0, bs)
 	if err != nil {
 		test.Errorf("Error writing to file")
 	}
 
 	readBs := make([]byte, 4)
-	globals.FileHandler.Read(testFile, 0, readBs)
+	fh.Read(testFile, 0, readBs)
 	for i := 0; i < len(bs); i++ {
 		if bs[i] != readBs[i] {
 			test.Errorf("Read values mismatch")
@@ -36,13 +36,13 @@ func TestFileReadWrite(test *testing.T) {
 	}
 
 	bs = []byte{79, 11, 254, 98}
-	err = globals.FileHandler.Write(testFile, 1, bs)
+	err = fh.Write(testFile, 1, bs)
 	if err != nil {
 		test.Errorf("Error writing to file")
 	}
 
 	readBs = make([]byte, 4)
-	globals.FileHandler.Read(testFile, 1, readBs)
+	fh.Read(testFile, 1, readBs)
 	for i := 0; i < len(bs); i++ {
 		if bs[i] != readBs[i] {
 			test.Errorf("Read values mismatch")
@@ -59,7 +59,7 @@ func TestIdReading(test *testing.T) {
 	defer testFile.Close()
 	defer os.Remove(testFile.Name())
 	testFile.WriteString(fmt.Sprintf("%d\n%d", 12, 17))
-	id, err := globals.FileHandler.ReadId(testFile)
+	id, err := fh.ReadId(testFile)
 	if err != nil {
 		test.Errorf("Error in reading id")
 	}
@@ -68,7 +68,7 @@ func TestIdReading(test *testing.T) {
 		test.Errorf("Id value mismatch")
 	}
 
-	id, err = globals.FileHandler.ReadId(testFile)
+	id, err = fh.ReadId(testFile)
 	if err != nil {
 		test.Errorf("Error in reading id")
 	}
