9471b21ae9e2d04f24ba6a3a1721dadba4bc9b79
olejeglejeg <oleg-taizov@yandex.ru>
2018-05-06 19:29:07 +0300

    - fixes

--- internal/app/core/master_node.go
+++ internal/app/core/master_node.go
@@ -1,8 +1,8 @@
 package core
 
 import (
-	"net/rpc"
 	"encoding/json"
+	"net/rpc"
 	"log"
 	"time"
 )
@@ -23,7 +23,7 @@ func sendPing(slave *Entity)  {
 
 func getSlavesIps() ([]string, error) {
 	var ips []string
-	var ipsJson = string("[\"10.240.18.30:8080\"]")
+	var ipsJson = string("[\"10.91.40.48:5000\"]")
 	err := json.Unmarshal([]byte(ipsJson), &ips)
 	return ips, err
 }
@@ -34,33 +34,39 @@ func Test() {
 		println("My IP: ", myIp)
 	}
 
-	master = initMaster(myIp, "8080")
+	master = initMaster(myIp, "7000")
 	initSlaves(&master)
 
 	for _, slave := range master.slaves {
+		// RPC call...
 		var rpcClient *rpc.Client
+		//var err error
 		attempt := 1
 		for attempt != -1 {
 			log.Printf("Try to connect (attempt %d) to %s", attempt, slave.ip)
 			attempt = attempt + 1
 			c := make(chan error, 1)
 			go func() {
-				rpcClient, err = rpc.DialHTTP("tcp", slave.ip + ":" + slave.port)
+				rpcClient, err = rpc.DialHTTP("tcp", slave.ip + ":7000")
+				if err == nil {
+					println("GG WP")
+				}
 				c <- err
 			}()
 			select {
 			case err := <-c:
 				if err != nil {
-					log.Print("Dialing:", err)
+					log.Print("dialing:", err)
 					time.Sleep(time.Second)
 				} else {
 					attempt = -1
 				}
 			case <-time.After(time.Second * 5):
-				println("Timeout...")
+				println("timeout...")
 			}
 		}
 	}
+
 
 	for _, slave := range master.slaves {
 		sendPing(&slave)
