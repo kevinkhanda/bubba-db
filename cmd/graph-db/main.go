1961f51e5358f3b1c016a9e043fcbbcade955c25
Your Name <sergey_umnov@mail.ru>
2018-05-07 00:45:07 +0300

    Add Label tests

--- cmd/graph-db/main.go
+++ cmd/graph-db/main.go
@@ -3,10 +3,6 @@ package main
 import (
 	"graph-db/internal/app/core"
 	"graph-db/internal/pkg/utils"
-	"net/rpc"
-	"net"
-	"log"
-	"net/http"
 )
 
 func main() {
@@ -14,18 +10,14 @@ func main() {
 	err := core.SwitchDb("asd")
 	utils.CheckError(err)
 
-	arith := new(core.Entity)
-	rpc.Register(arith)
-	rpc.HandleHTTP()
-	l, e := net.Listen("tcp", ":7000")
-	if e != nil {
-		log.Fatal("listen error:", e)
-	}
-	go http.Serve(l, nil)
+	//arith := new(core.Entity)
+	//rpc.Register(arith)
+	//rpc.HandleHTTP()
+	//l, e := net.Listen("tcp", ":7000")
+	//if e != nil {
+	//	log.Fatal("listen error:", e)
+	//}
+	//go http.Serve(l, nil)
 
-	for i := 0; i < 2342423423424; i++ {
-
-	}
-
-	//core.Test()
+	core.Test()
 }
